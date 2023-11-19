1b9da20da8e34952d25cbd90c0d3decf05236b65
hsmtkk <hsmtkk@gmail.com>
2023-11-19 09:26:53 +0900

    up

--- handle_get.py
+++
@@ -1,9 +0,0 @@
-def lambda_handler(event, context):
-    with open("index.html") as source_file:
-        source_code = source_file.read()
-
-    return {
-        "statusCode": 200,
-        "body": source_code,
-        "headers": {"Content-Type": "text/html"},
-    }
